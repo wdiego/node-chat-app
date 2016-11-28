cd8d9a4b14fd6041f0918ecf1eef3b3c98595fd6
Wanderson Diego <wdiegomt@gmail.com>
2016-11-28 17:16:36 -0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'wdiego',
-        text: 'Hey! How are you doing?'
-    });
-
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'wdiego@example.com',
-        text: 'Hey there!',
-        createdAt: 12345
-    });
-
     socket.on('createMessage', (message) => {
         console.log('New message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
