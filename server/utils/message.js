0b93022e834335dfa0cd54ac881760c85e0c1ba0
Wanderson Diego <wdiegomt@gmail.com>
2016-11-28 20:41:21 -0200

    Format timestamps using momentjs

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,18 @@
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
-    return { from, text, createdAt: new Date().getTime() };
+    return {
+        from,
+        text,
+        createdAt: moment().valueOf()
+    };
 };
 
 var generateLocationMessage = (from, lat, lng) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${lat},${lng}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     };
 };
 
